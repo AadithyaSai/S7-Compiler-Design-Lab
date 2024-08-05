73c2c352dc843a28e6040326c531b47a7f8c3e48
Aadithya Sai <aadithyas0610@gmail.com>
2024-08-05 15:17:50 +0530

    Update and rename input.c to source.c

---
+++ Programs/Tokenizer/source.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Hello World Program */
+
+int main() {
+	printf("Hello World\n");
+	int a = 5;
+	int b = 10;
+	
+	printf("Sum of 5 and 10 is: %d\n", a+b);  // Sum
+	
+	return 0;
+}
