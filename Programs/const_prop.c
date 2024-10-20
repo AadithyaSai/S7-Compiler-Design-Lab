53cc67f052255eceb1f88c900a728ed3db858592
Aadithya Sai <aadithyas0610@gmail.com>
2024-10-20 13:45:43 +0530

    Shortened Constant Propogation

--- Programs/const_prop.c
+++ Programs/const_prop.c
@@ -4,84 +4,74 @@
 #include <stdlib.h>
 
 void input();
-void change(int p, char *res);
 void output();
 void constant();
+void change(int result, char target[]);
 
 struct expr {
-    char op[2], op1[5], op2[5], res[5];
-    int flag;
+	char op[2], op1[5], op2[5], target[5];
+	int flag;
 } arr[10];
 int n;
 
 int main() {
-    input();
-    constant();
-    output();
+	input();
+	constant();
+	output();
 }
 
 void input() {
-    int i;
-    printf("Enter the maximum number of expressions : ");
-    scanf("%d", &n);
-    printf("\nEnter the input: \n");
-    for (i = 0; i < n; i++) {
-        scanf("%s", arr[i].op);
-        scanf("%s", arr[i].op1);
-        scanf("%s", arr[i].op2);
-        scanf("%s", arr[i].res);
-        arr[i].flag = 0;
-    }
-}
-
-void constant() {
-    int i;
-    int op1, op2, res;
-    char op, res1[5];
-    for (i = 0; i < n; i++) {
-        if ((isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0])) || strcmp(arr[i].op, "=") == 0) {
-            op1 = atoi(arr[i].op1);
-            op2 = atoi(arr[i].op2);
-            op = arr[i].op[0];
-            switch (op) {
-            case '+':
-                res = op1 + op2;
-                break;
-            case '-':
-                res = op1 - op2;
-                break;
-            case '*':
-                res = op1 * op2;
-                break;
-            case '/':
-                res = op1 / op2;
-                break;
-            case '=':
-                res = op1;
-                break;
-            }
-            sprintf(res1, "%d", res);
-            arr[i].flag = 1;
-            change(i, res1);
-        }
-    }
+	printf("Enter number of expressions: \n");
+	scanf("%d", &n);
+	printf("Enter the input: \n");
+	for (int i = 0; i < n; i++) {
+		scanf("%s %s %s %s", arr[i].op, arr[i].op1, arr[i].op2, arr[i].target);
+		arr[i].flag = 0;
+	}
 }
 
 void output() {
-    int i = 0;
-    printf("\nOptimized code is:\n");
-    for (i = 0; i < n; i++) {
-        if (!arr[i].flag)
-            printf("%s %s %s %s\n", arr[i].op, arr[i].op1, arr[i].op2, arr[i].res);
-    }
+	printf("Optimized code is:\n");
+	for (int i = 0; i < n; i++) {
+		if (!arr[i].flag)
+			printf("%s %s %s %s\n", arr[i].op, arr[i].op1, arr[i].op2, arr[i].target);
+	}
 }
 
-void change(int p, char *res) {
-    int i;
-    for (i = p + 1; i < n; i++) {
-        if (strcmp(arr[p].res, arr[i].op1) == 0)
-            strcpy(arr[i].op1, res);
-        else if (strcmp(arr[p].res, arr[i].op2) == 0)
-            strcpy(arr[i].op2, res);
-    }
+void constant() {
+	for (int i = 0; i < n; i++) {
+		if (strcmp(arr[i].op, "=") == 0 || (isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0]))) {
+			int a = atoi(arr[i].op1);
+			int b = atoi(arr[i].op2);
+			int res;
+			switch (arr[i].op[0]) {
+			case '+':
+				res = a + b;
+				break;
+			case '-':
+				res = a - b;
+				break;
+			case '*':
+				res = a * b;
+				break;
+			case '/':
+				res = a / b;
+				break;
+			case '=':
+				res = a;
+				break;
+			}
+			change(res, arr[i].target);
+			arr[i].flag = 1;
+		}
+	}
+}
+
+void change(int result, char target[]) {
+	for (int i = 0; i < n; i++) {
+		if (strcmp(arr[i].op1, target) == 0)
+			sprintf(arr[i].op1, "%d", result);
+		if (strcmp(arr[i].op2, target) == 0)
+			sprintf(arr[i].op2, "%d", result);
+	}
 }
